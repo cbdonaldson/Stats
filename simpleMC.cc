335360d1bbb50cac88d149e90533517290b5739c
cbdonaldson <charlie.b.donaldson@gmail.com>
2017-11-06 13:55:18 +0100

    question 2

---
+++ simpleMC.cc
@@ -0,0 +1,43 @@
+// Program to illustrate use of ROOT random number and histogram classes
+// Glen Cowan, RHUL, Physics, November 2007
+
+#include <iostream>
+#include <cmath>
+#include <TH1D.h>
+#include <TFile.h>
+#include <TRandom3.h>
+
+using namespace std;
+
+int main(){
+
+// Open output file (apparently needs to be done before booking)
+
+  TFile* file = new TFile("simpleMC.root", "recreate");
+
+// Book histograms
+
+  TH1D* h_ac = new TH1D("h_ac", "sawtooth_ac pdf",  100,  0, 1.0);
+
+// Create a TRandom3 object to generate random numbers
+
+  int seed = 45647;
+  TRandom3* ran = new TRandom3(seed);
+
+// Generate some random numbers and fill histograms
+
+  const int numValues = 10000000;
+
+  for (int i=0; i<numValues; ++i){
+    double r = ran->Rndm();             // uniform in ]0,1]
+    double x = pow(r,0.5);
+    h_saw->Fill(x);
+  }
+
+// Store all histograms in the output file and close up
+
+  file->Write();
+  file->Close();
+
+  return 0;
+}
